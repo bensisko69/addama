68a6d116831266af8384bb6d5ee463e60f2b4428
Loic RENOUD-GRAPPIN <lrenoud-@ubuntu>
2016-04-20 16:58:14 +0200

    modifi settings

--- addama/settings.py
+++ addama/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'django_markdown',
     'bootstrap3',
 ]
